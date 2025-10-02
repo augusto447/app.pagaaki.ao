7dff1f97df5fb9b8b5ce7d015526a314c99925ae
augusto447 <augusto11swat@gmail.com>
2025-10-02 13:23:33 +0100

    feat:avatar and notications button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,21 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Bell } from 'lucide-react'
+
 export function Header() {
   return (
-    <header className="p-6 border-b border-b-neutral-300 min-w-full">
-      <header>header</header>
-    </header>
-  );
+    <div className=" flex items-center gap-6 justify-end p-4 border ">
+      <button className="text-neutral-700 cursor-pointer bg-gray-200 p-2 rounded-md">
+        {' '}
+        <Bell className="w-5 h-5 " />
+      </button>
+      <div className="flex items-center gap-2">
+        <Avatar className=" ">
+          <AvatarImage src="https://github.com/shadcn.png" />
+
+          <AvatarFallback>AM</AvatarFallback>
+        </Avatar>
+        <span>Augusto Manuel</span>
+      </div>
+    </div>
+  )
 }
--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,18 +1,18 @@
-import { Outlet } from 'react-router-dom';
-import { SideBar } from '../components/sidebar';
-import { Header } from '../components/header';
+import { Outlet } from 'react-router-dom'
+import { SideBar } from '../components/sidebar'
+import { Header } from '../components/header'
 
 export function DefaultLayout() {
   return (
     <main className="flex">
       <SideBar />
 
-      <div className="flex-1">
+      <div className="flex-1 justify-end">
         <Header />
         <div className="p-6">
           <Outlet />
         </div>
       </div>
     </main>
-  );
+  )
 }
